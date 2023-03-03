1e15aaac274f6eb73238393c29305035f28ffa51
nanfangzhe <nanfangzhe@qq.com>
2023-03-04 01:50:31 +0800

    v0.0.1

---
+++ .vitepress/theme/components/Comments.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="gitalk-container"></div>
+</template>
+<script lang="ts" setup>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+import { onMounted } from "vue";
+// import { useData } from "vitepress";
+// const relativePath = useData().page.value.relativePath;
+
+const gitalk = new Gitalk({
+  clientID: "c4894cdcbf39d7dbcfb5",
+  clientSecret: "90473f3323b7225c9420d3572ca521bb666c65ed",
+  repo: "tea-blog.github.io",
+  owner: "tea-blog",
+  admin: ["tea-blog"],
+  id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
+  language: "zh-CN",
+  distractionFreeMode: true, // Facebook-like distraction free mode
+});
+
+onMounted(() => {
+  gitalk.render("gitalk-container");
+});
+</script>
+<style scoped></style>
---
+++ .vitepress/theme/components/ShareCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="shareCard">
+    <h1 class="title">
+      <span>Hi! Here is Tea Blog 👋</span>
+    </h1>
+    <p class="description">
+      茶博客，一个休闲娱乐的小地方。
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  align-items: center;
+  font-size: 2.2em;
+  font-weight: bold;
+}
+.description {
+  margin: 1em 0;
+}
+</style>
